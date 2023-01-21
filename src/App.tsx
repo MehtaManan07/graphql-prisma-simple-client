d6cb4dce977ba69be386ef98cbe6b06e83c098ec
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2023-01-21 15:25:10 +0530

    done with codegen

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import UserDisplay from "./components/UserDisplay";
 import { User as IUser } from "./types";
+import { useQuery } from "urql";
+import { GetUsersDocument } from "./graphql/generated";
 
 function App() {
-  const users: IUser[] = [
-    {
-      name: "Prisma Fan",
-      messages: [
-        {
-          body: "Prisma rocks!!",
-        },
-        {
-          body: "Did I mention I love Prisma?",
-        },
-      ],
-    },
-  ];
+  const [results] = useQuery({ query: GetUsersDocument });
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
-      {users.map((user, i) => (
+      {results.data?.users.map((user, i) => (
         <UserDisplay user={user} key={i} />
       ))}
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createClient, Provider as UrqlProvider} from "urql";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const client = createClient({
+  url: import.meta.env.VITE_API_URL || "http://localhost:4000/graphql",
+});
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <UrqlProvider value={client}>
+      <App />
+    </UrqlProvider>
+  </React.StrictMode>
+);
